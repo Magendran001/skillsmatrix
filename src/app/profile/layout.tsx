9b621bde3a4d450db48470284e5d57443c0bf941
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2024-08-21 11:25:38 +0530

    done

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -4,6 +4,31 @@ export const metadata = {
   title: 'Profile',
   description: 'Manage your user profile, update personal information, and change your password on YourApp.',
   keywords: 'profile, user settings, personal information',
+  openGraph: {
+    title: 'Profile | YourApp',
+    description: 'Manage your user profile, update personal information, and change your password on YourApp.',
+    url: 'https://skillsmatrix.vercel.app/profile',
+    images: [
+      {
+        url: 'https://skillsmatrix.vercel.app/_next/image?url=%2Fassets%2Fmedia%2Favatars%2Fprofilepic.jpg&w=128&q=75',
+        width: 1200,
+        height: 630,
+        alt: 'Profile preview',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Profile | YourApp',
+    description: 'Manage your user profile, update personal information, and change your password on YourApp.',
+    images: [
+      {
+        url: 'https://skillsmatrix.vercel.app/_next/image?url=%2Fassets%2Fmedia%2Favatars%2Fprofilepic.jpg&w=128&q=75',
+        alt: 'Profile preview',
+      },
+    ],
+  },
 };
 
 
